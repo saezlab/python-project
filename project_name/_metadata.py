26f83b5204a34e6d5bd7c602719da87f20b2f570
deeenes <turei.denes@gmail.com>
2023-03-22 18:01:14 +0100

    added the `_metadata` module (used in other projects)

--- project_name/_metadata.py
+++ project_name/_metadata.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
 #
-# Copyright 2022, Saez Lab
+# This file is part of the `project_name` Python module
 #
-# File author(s): Denes Turei <turei.denes@gmail.com>
-#                 ...
+# Copyright 2023
+# Heidelberg University Hospital
 #
-# Distributed under GPLv3 license, see the file `LICENSE`.
+# File author(s): Denes Turei (turei.denes@gmail.com)
+#
+# Distributed under GPLv3 license
+# See the file `LICENSE` or read a copy at
+# http://www.gnu.org/licenses/gpl-3.0.txt
 #
 
 """
@@ -20,6 +24,8 @@ import pathlib
 import importlib.metadata
 
 import toml
+
+_VERSION = '0.0.1'
 
 
 def get_metadata():
@@ -54,7 +60,18 @@ def get_metadata():
 
     if not meta:
 
-        meta = dict(importlib.metadata.metadata(here.name).items())
+        try:
+
+            meta = {
+                k.lower(): v for k, v in
+                importlib.metadata.metadata(here.name).items()
+            }
+
+        except importlib.metadata.PackageNotFoundError:
+
+            pass
+
+    meta['version'] = meta.get('version', None) or _VERSION
 
     return meta
 
