20c64b06087640e57e985a3a04f789ffd30be609
deeenes <turei.denes@gmail.com>
2023-04-09 16:59:19 +0200

    template fields in `metadata` and `bumpversion` config

--- {{cookiecutter.project_name}}/project_name/_metadata.py
+++ {{cookiecutter.project_name}}/project_name/_metadata.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
 #
-# This file is part of the `project_name` Python module
+# This file is part of the `{{ cookiecutter.package_name }}` Python module
 #
 # Copyright 2023
 # Heidelberg University Hospital
 #
-# File author(s): Denes Turei (turei.denes@gmail.com)
+# File author(s): {{ cookiecutter.author }} ({{ cookiecutter.author_email }})
 #
-# Distributed under GPLv3 license
+# Distributed under {{ cookiecutter.license }} license
 # See the file `LICENSE` or read a copy at
 # http://www.gnu.org/licenses/gpl-3.0.txt
 #
@@ -25,7 +25,7 @@ import importlib.metadata
 
 import toml
 
-_VERSION = '0.1.0'
+_VERSION = '0.0.1'
 
 
 def get_metadata():
