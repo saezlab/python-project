97e4b1ab2fac2f19c43a9a9026195090bae2b85d
deeenes <turei.denes@gmail.com>
2023-04-10 04:28:40 +0200

    fill package file headers from the template

--- {{cookiecutter.project_slug}}/{{cookiecutter.package_name}}/_metadata.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.package_name}}/_metadata.py
@@ -3,14 +3,44 @@
 #
 # This file is part of the `{{ cookiecutter.package_name }}` Python module
 #
-# Copyright 2023
-# Heidelberg University Hospital
+# Copyright {{ cookiecutter.__year }}
+# {{ cookiecutter.copyright_holder }}
 #
 # File author(s): {{ cookiecutter.author_full_name }} ({{ cookiecutter.author_email }})
 #
-# Distributed under {{ cookiecutter.license }} license
+# Distributed under the {{ cookiecutter._licenses_short[cookiecutter.license] }} license
 # See the file `LICENSE` or read a copy at
-# http://www.gnu.org/licenses/gpl-3.0.txt
+
+{%- if cookiecutter.license == 'GNU General Public License Version 3' -%}
+
+# https://www.gnu.org/licenses/gpl-3.0.txt
+
+{%- elif cookiecutter.license == 'MIT License' -%}
+
+# https://opensource.org/license/mit
+
+{%- elif cookiecutter.license == 'Apache License Version 2.0' -%}
+
+# https://www.apache.org/licenses/LICENSE-2.0
+
+{%- elif cookiecutter.license == 'BSD 3-Clause License' -%}
+
+# https://opensource.org/license/bsd-3-clause
+
+{%- elif cookiecutter.license == 'BSD 2-Clause License' -%}
+
+# https://opensource.org/license/bsd-2-clause
+
+{%- elif cookiecutter.license == 'ISC License' -%}
+
+# https://opensource.org/license/isc-license-txt
+
+{%- elif cookiecutter.license == 'Unlicense' -%}
+
+# https://unlicense.org/
+
+{%- endif -%}
+
 #
 
 """
