af6a6f4ff0d0cc2da63f2ddcc54a69faa12030d0
deeenes <turei.denes@gmail.com>
2023-04-10 14:51:34 +0200

    syntax error in sphinx conf

--- {{cookiecutter.project_slug}}/docs/src/conf.py
+++ {{cookiecutter.project_slug}}/docs/src/conf.py
@@ -117,7 +117,7 @@ html_context = {
     'conf_py_path': '/docs/',  # Path in the checkout to the docs root
     'conf_py_path': '/docs/',  # Path in the checkout to the docs root
 }
-}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
